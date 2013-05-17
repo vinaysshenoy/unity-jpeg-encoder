b58358f1915ff01e6c78bd90c77e62f8b3ce1762
andreasbroager@gmail.com <andreasbroager@gmail.com@5b80ae90-e694-657c-91f9-eed538487d4e>
2013-05-17 16:11:30 +0000

    Cleanup

--- trunk/UnityProject/Assets/TestEncode.cs
+++ trunk/UnityProject/Assets/TestEncode.cs
@@ -55,35 +55,3 @@ public class TestEncode : MonoBehaviour
 	}
 	
 }
-
-
-
-///**
-//* Take the screen buffer and spit out a JPG
-//*/
-//function ScreenshotEncode()
-//{
-//	// wait for graphics to render
-//	yield WaitForEndOfFrame();
-//	
-//	// create a texture to pass to encoding
-//	var texture:Texture2D = new Texture2D (Screen.width, Screen.height, TextureFormat.RGB24, false);
-//	
-//	// put buffer into texture
-//	texture.ReadPixels(Rect(0.0, 0.0, Screen.width, Screen.height), 0.0, 0.0);
-//	texture.Apply();
-//
-//	// split the process up--ReadPixels() and the GetPixels() call inside of the encoder are both pretty heavy
-//	yield;
-//	
-//	// create our encoder for this texture
-//	var encoder:JPGEncoder = new JPGEncoder(texture, 75.0);
-//	
-//	// encoder is threaded; wait for it to finish
-//	while(!encoder.isDone)
-//		yield;
-//	
-//	// save our test image (could also upload to WWW)
-//	File.WriteAllBytes(Application.dataPath + "/../testscreen-" + count + ".jpg", encoder.GetBytes());
-//	count++;
-//}
